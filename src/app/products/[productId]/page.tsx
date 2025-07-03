730785fbbc20038abe20033fe90efe8e4ae33864
Baljan Kanatbek <baljann19@gmail.com>
2025-07-03 21:14:46 +0300

    Refactor ProductPage component to use a defined interface for props, improving type safety and clarity.

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,12 +1,12 @@
 import ProductClient from "./ProductClient";
 
-//vercel keeps giving this errors regardless defining interface or using inline type: //Type error: Type '{ params: { productId: string; }; }' does not satisfy the constraint 'PageProps'. //Types of property 'params' are incompatible. //Type '{ productId: string; }' is missing the following properties from type 'Promise<any>': then, catch, finally, [Symbol.toStringTag]
+interface ProductPageProps {
+  params: {
+    productId: string;
+  };
+}
 
-export default async function ProductPage({
-  params,
-}: {
-  params: { productId: string };
-}) {
+export default async function ProductPage({ params }: ProductPageProps) {
   const res = await fetch(`https://dummyjson.com/products/${params.productId}`);
   if (!res.ok) {
     return <div>Error loading product</div>;
